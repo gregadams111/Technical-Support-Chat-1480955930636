4337e7ae1dc912b29b810438ad75ce078323ea45
Greg Adams <gregadams111>
2016-12-05 12:23:33 -0600

    Adding bilogo

--- welcome.py
+++ welcome.py
@@ -21,24 +21,28 @@ app = Flask(__name__)
 def Welcome():
     return app.send_static_file('index.html')
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
+@app.route('/bilogo.png')
+def GetLogo():
+    return app.send_static_file('bilogo.png')
 
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
+# @app.route('/myapp')
+# def WelcomeToMyapp():
+#     return 'Welcome again to my app running on Bluemix!'
 
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
+# @app.route('/api/people')
+# def GetPeople():
+#     list = [
+#         {'name': 'John', 'age': 28},
+#         {'name': 'Bill', 'val': 26}
+#     ]
+#     return jsonify(results=list)
+
+# @app.route('/api/people/<name>')
+# def SayHello(name):
+#     message = {
+#         'message': 'Hello ' + name
+#     }
+#     return jsonify(results=message)
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
